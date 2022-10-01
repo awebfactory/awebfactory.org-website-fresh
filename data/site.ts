c8a03d71d0d350085ffebd109206e983284b0376
Victor Kane <victorkane@gmail.com>
2022-10-01 15:42:28 -0300

    build: site info data store

---
+++ data/site.ts
@@ -0,0 +1,10 @@
+export const site = {
+  title: "AWebFactory Project Flow &amp; Tracker",
+  description:
+    "An AWebFactory #BuildInPublic #LearnInPublic website built with Fresh",
+  // This should be a url to the static file you're using for your open graph image
+  // ogImage: "https://fresh-blog.littlesticks.dev/og-image.png",
+  copyrightName: "Victor Kane",
+  // The url of the source code for the site, used in the footer.
+  // viewSourceUrl: "https://github.com/littlesticks/fresh-blog",
+};
