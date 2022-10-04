1d97675954f03ce03ebb1e0c44626df6cc6a8ea6
Victor Kane <victorkane@gmail.com>
2022-10-04 13:56:03 -0300

    build: add layout to catch-all dynamic route

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -1,19 +1,35 @@
 import { PageProps } from "$fresh/server.ts";
+import { HeadElement } from "../components/HeadElement.tsx";
+import { NavBar } from "../components/NavBar.tsx";
+import { Footer } from "../components/Footer.tsx";
+import { site } from "../data/site.ts";
+import { Fragment } from "preact";
 
 // catch-all page
 export default function Greet(props: PageProps) {
   return (
-    <div class="p-4 mx-auto max-w-screen-md">
-      <p>
-        Hi there! Did you mean project{" "}
-        <span class="text-green-400">{props.params.name}?</span>
-      </p>
-      <p>
-        Sorry, no project of that name. But, click to see a{" "}
-        <a href="/api/project" class="bg-yellow-100 hover:bg-yellow-200">
-          Random Project
-        </a>
-      </p>
-    </div>
+    <Fragment>
+      <HeadElement
+        title={`${props.params.name} | ${site.title}`}
+        description=""
+        url={props.url}
+      />
+      <NavBar />
+      <div class="p-4 mx-auto max-w-screen-md">
+        <p>
+          Hi there! Did you mean project{" "}
+          <span class="text-green-400">
+            {`${props.params.name} | ${site.title}`}
+          </span>
+        </p>
+        <p>
+          Sorry, no project of that name. But, click to see a{" "}
+          <a href="/api/project" class="bg-yellow-100 hover:bg-yellow-200">
+            Random Project
+          </a>
+        </p>
+      </div>
+      <Footer />
+    </Fragment>
   );
 }
