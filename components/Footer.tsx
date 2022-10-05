878711a08aafa0569ab42bc79921ecb806d7c6b8
Victor Kane <victorkane@gmail.com>
2022-10-05 05:11:21 -0300

    build: add navigation to simple Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,95 @@ import { site } from "../data/site.ts";
 
 export function Footer() {
   return (
-    <footer>
-      <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
-        <div class="flex justify-center space-x-6 md:order-2">
+    <footer class="bg-white">
+      <div class="mx-auto max-w-7xl overflow-hidden py-12 px-4 sm:px-6 lg:px-8">
+        <nav
+          class="-mx-5 -my-2 flex flex-wrap justify-center"
+          aria-label="Footer"
+        >
+          <div class="px-5 py-2">
+            <a href="/" class="text-base text-gray-500 hover:text-gray-900">
+              <img
+                src="/icon170.png"
+                class="w-12 h-12"
+                alt="AWebFactory"
+              />
+            </a>
+          </div>
+
+          <div class="px-5 py-2">
+            <a
+              href="/project"
+              class="text-base text-gray-500 hover:text-gray-900"
+            >
+              Projects
+            </a>
+          </div>
+
+          <div class="px-5 py-2">
+            <a
+              href="/process"
+              class="text-base text-gray-500 hover:text-gray-900"
+            >
+              Process
+            </a>
+          </div>
+
+          <div class="px-5 py-2">
+            <a
+              href="/resource"
+              class="text-base text-gray-500 hover:text-gray-900"
+            >
+              Resources
+            </a>
+          </div>
+
+          <div class="px-5 py-2">
+            <a
+              href="/about"
+              class="text-base text-gray-500 hover:text-gray-900"
+            >
+              About
+            </a>
+          </div>
+
+          <div class="px-5 py-2">
+            <a
+              href="/settings"
+              class="text-base text-gray-500 hover:text-gray-900"
+            >
+              Settings
+            </a>
+          </div>
+
+          <div class="px-5 py-2">
+            <a
+              href="/privacy"
+              class="text-base text-gray-500 hover:text-gray-900"
+            >
+              Privacy
+            </a>
+          </div>
+
+          <div class="px-5 py-2">
+            <a
+              href="/thank-you"
+              class="text-base text-gray-500 hover:text-gray-900"
+            >
+              Thank you
+            </a>
+          </div>
+
+          <div class="px-5 py-2">
+            <a
+              href="/colophon"
+              class="text-base text-gray-500 hover:text-gray-900"
+            >
+              Colophon
+            </a>
+          </div>
+        </nav>
+        <div class="mt-8 flex justify-center space-x-6">
           <a
             href="https://www.facebook.com/AWebFactory"
             class="text-gray-400 hover:text-gray-500"
@@ -23,6 +109,29 @@ export function Footer() {
               />
             </svg>
           </a>
+
+          {
+            /*
+
+          <a href="#" class="text-gray-400 hover:text-gray-500">
+            <span class="sr-only">Instagram</span>
+            <svg
+              class="h-6 w-6"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </a>
+
+            */
+          }
+
           <a
             href="https://twitter.com/a_web_factory"
             class="text-gray-400 hover:text-gray-500"
@@ -37,6 +146,7 @@ export function Footer() {
               <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
             </svg>
           </a>
+
           <a
             href="https://github.com/awebfactory"
             class="text-gray-400 hover:text-gray-500"
@@ -64,13 +174,17 @@ export function Footer() {
             <svg
               class="h-6 w-6"
               fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 512"
+              aria-hidden="true"
             >
-              {/* Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}
-              <path d="M186.8 202.1l95.2 54.1-95.2 54.1V202.1zM448 80v352c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V80c0-26.5 21.5-48 48-48h352c26.5 0 48 21.5 48 48zm-42 176.3s0-59.6-7.6-88.2c-4.2-15.8-16.5-28.2-32.2-32.4C337.9 128 224 128 224 128s-113.9 0-142.2 7.7c-15.7 4.2-28 16.6-32.2 32.4-7.6 28.5-7.6 88.2-7.6 88.2s0 59.6 7.6 88.2c4.2 15.8 16.5 27.7 32.2 31.9C110.1 384 224 384 224 384s113.9 0 142.2-7.7c15.7-4.2 28-16.1 32.2-31.9 7.6-28.5 7.6-88.1 7.6-88.1z" />
+              <path
+                fill-rule="evenodd"
+                d="M186.8 202.1l95.2 54.1-95.2 54.1V202.1zM448 80v352c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V80c0-26.5 21.5-48 48-48h352c26.5 0 48 21.5 48 48zm-42 176.3s0-59.6-7.6-88.2c-4.2-15.8-16.5-28.2-32.2-32.4C337.9 128 224 128 224 128s-113.9 0-142.2 7.7c-15.7 4.2-28 16.6-32.2 32.4-7.6 28.5-7.6 88.2-7.6 88.2s0 59.6 7.6 88.2c4.2 15.8 16.5 27.7 32.2 31.9C110.1 384 224 384 224 384s113.9 0 142.2-7.7c15.7-4.2 28-16.1 32.2-31.9 7.6-28.5 7.6-88.1 7.6-88.1z"
+                clip-rule="evenodd"
+              />
             </svg>
           </a>
+
           <a
             href="https://www.twitch.tv/awebfactory"
             class="text-gray-400 hover:text-gray-500"
@@ -79,20 +193,21 @@ export function Footer() {
             <svg
               class="h-6 w-6"
               fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 512"
+              aria-hidden="true"
             >
-              {/* Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}
-              <path d="M391.17,103.47H352.54v109.7h38.63ZM285,103H246.37V212.75H285ZM120.83,0,24.31,91.42V420.58H140.14V512l96.53-91.42h77.25L487.69,256V0ZM449.07,237.75l-77.22,73.12H294.61l-67.6,64v-64H140.14V36.58H449.07Z" />
+              <path
+                fill-rule="evenodd"
+                d="M391.17,103.47H352.54v109.7h38.63ZM285,103H246.37V212.75H285ZM120.83,0,24.31,91.42V420.58H140.14V512l96.53-91.42h77.25L487.69,256V0ZM449.07,237.75l-77.22,73.12H294.61l-67.6,64v-64H140.14V36.58H449.07Z"
+                clip-rule="evenodd"
+              />
             </svg>
           </a>
         </div>
-        <div class="mt-8 md:mt-0 md:order-1">
-          <p class="text-center text-base text-gray-400">
-            &copy; 2006-{new Date().getFullYear()} {site.copyrightName}.{"  "}
-            All rights reserved.
-          </p>
-        </div>
+        <p class="mt-8 text-center text-base text-gray-400">
+          &copy; 2006-{new Date().getFullYear()} {site.copyrightName}.{"  "}
+          All rights reserved.
+        </p>
       </div>
     </footer>
   );
