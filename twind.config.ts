6d7b4f27ba0232d66eab8adf7e042399fb82a4da
Victor Kane <victorkane@gmail.com>
2022-10-06 07:21:03 -0300

    style(theme): initial theme local fonts setup and test

--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,34 @@
 import { Options } from "$fresh/plugins/twind.ts";
+import { apply } from "twind";
 
 export default {
   selfURL: import.meta.url,
+  preflight: {
+    "@font-face": [
+      {
+        fontFamily: "Crete Round",
+        fontWeight: "normal",
+        fontStyle: "normal",
+        src:
+          'url("/fonts/creteround/CreteRound-Regular.ttf") format("truetype")',
+      },
+      {
+        fontFamily: "PT Sans",
+        fontWeight: "normal",
+        fontStyle: "normal",
+        src: 'url("/fonts/ptsans/PT_Sans-Web-Regular.ttf") format("truetype")',
+      },
+    ],
+    h2: apply`font-heading`,
+    p: apply`font-content`,
+    nav: apply`font-heading`,
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: "Crete Round, Geneva, Arial, Helvetica, sans-serif",
+        content: "PT Sans, Verdana, Arial, Helvetica, sans-serif",
+      },
+    },
+  },
 } as Options;
